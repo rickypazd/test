b4da3bff401e72b1e8ff97ccf8278ef81d0b5630
rickypazd <35882906+rickypazd@users.noreply.github.com>
2021-11-19 19:14:39 -0400

    as

--- src/App.java
+++ src/App.java
@@ -6,13 +6,13 @@ import java.util.*;
 import Servisofts.Servisofts;
 
 public class App {
- 
+
     public static String getSystemIP() {
         try {
             String sysIP = "";
             String OSName = System.getProperty("os.name");
             if (OSName.contains("Windows")) {
-                sysIP = "ip windows : "+ InetAddress.getLocalHost().getHostAddress();
+                sysIP = "ip windows : " + InetAddress.getLocalHost().getHostAddress();
             } else {
                 sysIP = getIpLinux();
 
@@ -28,7 +28,7 @@ public class App {
 
         InetAddress ip = InetAddress.getLocalHost();
         String ipAddress = ip.getHostAddress();
-        return "ip linux : "+ipAddress;
+        return "ip linux : " + ipAddress;
 
     }
 
@@ -70,64 +70,6 @@ public class App {
         // String ipA = getLocalHostLANAddress().getHostAddress();
         // System.out.println("IP: " + ipA);
 
-    }
-
-    private static InetAddress getLocalHostLANAddress() throws UnknownHostException {
-        try {
-            InetAddress candidateAddress = null;
-            // Iterate all NICs (network interface cards)...
-            for (Enumeration ifaces = NetworkInterface.getNetworkInterfaces(); ifaces.hasMoreElements();) {
-                NetworkInterface iface = (NetworkInterface) ifaces.nextElement();
-                // Iterate all IP addresses assigned to each card...
-                for (Enumeration inetAddrs = iface.getInetAddresses(); inetAddrs.hasMoreElements();) {
-                    InetAddress inetAddr = (InetAddress) inetAddrs.nextElement();
-                    if (!inetAddr.isLoopbackAddress()) {
-
-                        if (inetAddr.isSiteLocalAddress()) {
-                            // Found non-loopback site-local address. Return it immediately...
-                            return inetAddr;
-                        } else if (candidateAddress == null) {
-                            // Found non-loopback address, but not necessarily site-local.
-                            // Store it as a candidate to be returned if site-local address is not
-                            // subsequently found...
-                            candidateAddress = inetAddr;
-                            // Note that we don't repeatedly assign non-loopback non-site-local addresses as
-                            // candidates,
-                            // only the first. For subsequent iterations, candidate will be non-null.
-                        }
-                    }
-                }
- 
-    public static void main(String[] args) throws Exception {
-        System.out.println("TEST -- 1");
-        test1();
-        System.out.println("TEST -- 2");
-        test2();
-    }
-
-    public static void test1() throws SocketException {
-        Enumeration<NetworkInterface> networkInterfaceEnumeration = NetworkInterface.getNetworkInterfaces();
-        while (networkInterfaceEnumeration.hasMoreElements()) {
-            for (InterfaceAddress interfaceAddress : networkInterfaceEnumeration.nextElement().getInterfaceAddresses())
-                if (interfaceAddress.getAddress().isSiteLocalAddress())
-                    System.out.println(interfaceAddress.getAddress().getHostAddress());
-        }
-    }
-
-    public static void test2() throws SocketException, UnknownHostException {
-        System.out.println("Your Host addr: " + InetAddress.getLocalHost().getHostAddress()); // often returns
-        Enumeration<NetworkInterface> n = NetworkInterface.getNetworkInterfaces();
-        for (; n.hasMoreElements();) {
-            NetworkInterface e = n.nextElement();
-
-            Enumeration<InetAddress> a = e.getInetAddresses();
-            for (; a.hasMoreElements();) {
-                InetAddress addr = a.nextElement();
-                System.out.println("  " + addr.getHostAddress());
-                System.out.println("  " + e.getDisplayName());
-
-             }
-        }
     }
 
 }
